f30da4fe37bec22927828f6c6eb547f13707f4cb
Jarko Dubbeldam <jarkodubbeldam@gmail.com>
2018-10-29 18:44:19 +0100

    eerste drie requirements aflegstapel af.

---
+++ EvolutionModel/Aflegstapel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evolution.Model {
+  public class Aflegstapel {
+    private Stack<Card> cards;
+
+    public Aflegstapel() {
+      cards = new Stack<Card>(129);
+    }
+
+    public int Count => cards.Count;
+
+    public void Add(Card kaart) {
+      cards.Push(kaart);
+    }
+  }
+}
---
+++ EvolutionModelTests/AflegstapelTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Evolution.Model;
+
+namespace EvolutionModelTests {
+  [TestClass]
+  public class AflegstapelTests {
+    [TestMethod]
+    public void AflegstapelBegintLeeg() {
+      var aflegstapel = new Aflegstapel();
+      
+      var expectedCount = 0;
+      var actualCount = aflegstapel.Count;
+
+      Assert.AreEqual(expectedCount, actualCount);
+    }
+
+    [TestMethod]
+    public void CanAddCardsToAflegStapel() {
+      var aflegstapel = new Aflegstapel();
+
+      Card kaart = new Card();
+
+      aflegstapel.Add(kaart);
+    }
+
+    [TestMethod]
+    public void IfACardIsAddedCountIncreases() {
+      var aflegstapel = new Aflegstapel();
+      
+      for(var cardAmount = 1; cardAmount < 20; cardAmount++) {
+        var newcard = new Card();
+        aflegstapel.Add(newcard);
+        var actualCount = aflegstapel.Count;
+
+        Assert.AreEqual(cardAmount, actualCount);
+      }
+    }
+  }
+}
