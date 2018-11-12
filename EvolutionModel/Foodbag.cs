5f6beef40853338c8678f1c9009c3ba7166708c3
Mariska Oudshoorn <Mariska.oudshoorn@hotmail.com>
2018-11-12 22:11:25 +0100

    Begin gemaakt aan foodbag

---
+++ EvolutionModel/Foodbag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evolution.Model {
+    public class Foodbag {
+        private int foodamount;
+
+        public Foodbag(){
+            foodamount = 0;
+        }
+    }
+}
---
+++ EvolutionModelTests/FoodbagTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Evolution.Model;
+
+namespace EvolutionModelTests{
+    [TestClass]
+    public class FoodbagTests{
+        [TestMethod]
+        public void FoodbagBegintLeeg(){
+            var foodbag = new Foodbag();
+
+            var expectedCount = 0;
+            var actualCount = foodbag.Count;
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+    }
+}
