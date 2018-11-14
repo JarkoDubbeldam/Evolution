af7d2ff0d98c03ef272aebe6d06b22b9d556ab9c
MariskaOudshoorn <43948167+MariskaOudshoorn@users.noreply.github.com>
2018-11-14 12:26:09 +0100

    Foodbag (#10)
    * Begin gemaakt aan foodbag
    
    * Foodbag begint leeg, en begin kaneten erin stoppen
    
    * verwijder rare file :S
    
    * Foodbag begint leeg, en er kan eten toegevoegd worden aan de foodbag
    
    * Removed count, added overflowcheck and overflowtest
    
    * Changed names

---
+++ EvolutionModel/Foodbag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evolution.Model {
+    public class Foodbag {
+        public uint Foodamount {
+            get;
+            private set;
+        }
+
+        public Foodbag(){ 
+            Foodamount = 0;
+        }
+
+        public void AddFood(uint additionalFood){
+            checked
+            {
+                Foodamount += additionalFood;
+            } 
+        }
+    }
+}
+
---
+++ EvolutionModelTests/FoodbagTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Evolution.Model;
+
+namespace EvolutionModelTests{
+    [TestClass]
+    public class FoodbagTests{
+        [TestMethod]
+        public void FoodbagConstructionStartsWithEmptyFoodbag(){
+            var foodbag = new Foodbag();
+
+            uint expectedCount = 0;
+            var actualCount = foodbag.Foodamount;
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+        [TestMethod]
+        public void FoodbagAddFoodIncreasesFoodamountWithPassedValue(){
+            var foodbag = new Foodbag();
+            foodbag.AddFood(5);
+            uint check = 5;
+            Assert.AreEqual(foodbag.Foodamount, check);
+
+        }
+        [TestMethod]
+        public void FoodbagAddFoodThrowsExceptionWhenFoodamountOverflows() {
+            var bag = new Foodbag();
+            bag.AddFood(1); // Foodbag now contains a positive amount of food.
+            Assert.ThrowsException<OverflowException>(() => bag.AddFood(uint.MaxValue));
+        }
+    }
+}
