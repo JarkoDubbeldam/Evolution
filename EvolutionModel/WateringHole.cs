cf163891c0cf667e9a7270ae9e7c4dc739c739d1
m.oudshoorn <m.oudshoorn@LT2020>
2018-11-14 13:32:58 +0100

    Added wateringhole files, added wateringholestartsempty test

---
+++ EvolutionModel/WateringHole.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evolution.Model
+{
+    class WateringHole{
+        public uint Foodamount {
+            get;
+            private set;
+        }
+
+        public WateringHole() {
+            Foodamount = 0;
+        }
+    }
+}
---
+++ EvolutionModelTests/WateringHoleTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Evolution.Model;
+
+namespace EvolutionModelTests {
+    [TestClass]
+    public class WateringHoleTests {
+        [TestMethod]
+        public void WateringHoleConstructionStartsEmpty() {
+            var wateringhole = new WateringHole();
+
+            var expectedCount = 0;
+            var actualCount = wateringhole.foodamount;
+
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+    
+    }
+}
