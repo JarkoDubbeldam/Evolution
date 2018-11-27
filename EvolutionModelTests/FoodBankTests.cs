577056604d281b96260b67823b3fd983afdc479d
m.oudshoorn <m.oudshoorn@LT2020>
2018-11-27 14:17:03 +0100

    Begin van de FoodBank

---
+++ EvolutionModelTests/FoodBankTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Evolution.Model;
+
+namespace EvolutionModelTests{
+    [TestClass]
+    public class FoodBankTests
+    {
+        [TestMethod]
+        public void FoodBankContainsFood()
+        {
+
+        }
+
+        [TestMethod]
+        public void FoodBankCanBeEatenBySpecies()
+        {
+
+        }
+
+        [TestMethod]
+        public void FoodBankCanBeEatenByTrait()
+        {
+
+        }
+
+    }
+}
