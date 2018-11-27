13248e377c40be53dc04cc780617842046303057
m.oudshoorn <m.oudshoorn@LT2020>
2018-11-27 13:18:09 +0100

    Species en testspecies aangemaakt. Begin gemaakt aan testen opschrijven.

---
+++ EvolutionModelTests/SpeciesTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Evolution.Model;
+
+namespace EvolutionModelTests
+{
+    [TestClass]
+    public class SpeciesTests
+    {
+        [TestMethod]
+        public void SpeciesBegintLeeg()
+        {
+
+        }
+
+        [TestMethod]
+        public void SpeciesCanEatFromWateringHole()
+        {
+
+        }
+
+        [TestMethod]
+        public void SpeciesCanAddPopulation()
+        {
+
+        }
+
+        [TestMethod]
+        public void SpeciesCanAddSize()
+        {
+
+        }
+
+        [TestMethod]
+        public void SpeciesCanDecreasePopulation()
+        {
+
+        }
+
+        [TestMethod]
+        public void SpeciesDiesIfPopulationIsZero()
+        {
+
+        }
+
+        [TestMethod]
+        public void SpeciesCanPutFoodInBag()
+        {
+
+        }
+
+        [TestMethod]
+        public void SpeciesDecreasesIfHungry()
+        {
+
+        }
+
+        [TestMethod]
+        public void SpeciesCanEatOtherSpecies()
+        {
+
+        }
+
+        [TestMethod]
+        public void SpeciesCantEatBiggerSpecies()
+        {
+
+        }
+
+        [TestMethod]
+        public void SpeciesDecreasesIfEaten()
+        {
+
+        }
+
+        [TestMethod]
+        public void SpeciesCantEatMoreThanPopulation()
+        {
+
+        }
+
+        [TestMethod]
+        public void SpeciesCanEatFromFoodbank()
+        {
+
+        }
+
+        [TestMethod]
+        public void SpeciesCanHaveTraits()
+        {
+
+        }
+
+        [TestMethod]
+        public void SpeciesCanRemoveTraits()
+        {
+
+        }
+
+    }
+}
