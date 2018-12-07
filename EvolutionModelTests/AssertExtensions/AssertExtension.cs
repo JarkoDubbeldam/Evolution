50d7735736ff1fe3cf20c733dfa53d03574b0bac
Jarko Dubbeldam <jarkodubbeldam@gmail.com>
2018-12-07 18:36:44 +0100

    Docstring

--- EvolutionModelTests/AssertExtensions/AssertExtension.cs
+++ EvolutionModelTests/AssertExtensions/AssertExtension.cs
@@ -6,10 +6,21 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace EvolutionModelTests.AssertExtensions {
   static class AssertExtension {
+    /// <summary>
+    /// Tests whether the specified event triggers during the action.
+    /// </summary>
+    /// <param name="subscriptor">Method used to subscribe an eventhandler.</param>
+    /// <param name="action">Action that triggers the event.</param>
     public static void TriggersEvent(Action<EventHandler> subscriptor, Action action) {
       TriggersEvent(subscriptor, action, "Action did not trigger the event.");
     }
 
+    /// <summary>
+    /// Tests whether the specified event triggers during the action.
+    /// </summary>
+    /// <param name="subscriptor">Method used to subscribe an eventhandler.</param>
+    /// <param name="action">Action that triggers the event.</param>
+    /// <param name="message">Message to display on failure.</param>
     public static void TriggersEvent(Action<EventHandler> subscriptor, Action action, string message) {
       void EventTester(object sender, EventArgs args) {
         throw new EventTriggeredException();
@@ -25,10 +36,23 @@ namespace EvolutionModelTests.AssertExtensions {
       Assert.Fail("Event did not trigger on action.");
     }
 
+    /// <summary>
+    /// Tests whether the specified event triggers during the action.
+    /// </summary>
+    /// <typeparam name="TEventArgs">EventArgs type used.</typeparam>
+    /// <param name="subscriptor">Method used to subscribe an eventhandler.</param>
+    /// <param name="action">Action that triggers the event.</param>
     public static void TriggersEvent<TEventArgs>(Action<EventHandler<TEventArgs>> subscriptor, Action action) {
       TriggersEvent(subscriptor, action, "Action did not trigger the event");
     }
 
+    /// <summary>
+    /// Tests whether the specified event triggers during the action.
+    /// </summary>
+    /// <typeparam name="TEventArgs">EventArgs type used.</typeparam>
+    /// <param name="subscriptor">Method used to subscribe an eventhandler.</param>
+    /// <param name="action">Action that triggers the event.</param>
+    /// <param name="message">Message to display on failure.</param>
     public static void TriggersEvent<TEventArgs>(Action<EventHandler<TEventArgs>> subscriptor, Action action, string message) {
       void EventTester(object sender, TEventArgs args) {
         throw new EventTriggeredException();
