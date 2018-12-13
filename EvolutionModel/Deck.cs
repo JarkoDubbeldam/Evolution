e5b8dc35fa3f6bc87bbe8e4d3a32ba801f78f206
Mariska Oudshoorn <Mariska.oudshoorn@hotmail.com>
2018-12-13 14:37:30 +0100

    added deck class and deck testclass

---
+++ EvolutionModel/Deck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evolution.Model
+{
+    public class Deck{
+        private Stack<Card> Cards;
+
+        public Deck(){
+            cards = new Stack<Card>(129);
+        }
+
+    }
+}
---
+++ EvolutionModelTests/DeckTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Evolution.Model;
+
+namespace EvolutionModelTests {
+
+    [TestClass]
+    public class DeckTests{
+        [TestMethod]
+        public void DeckBegintVol() {
+            var deck = new Deck();
+            var expectedCount = 129;
+            var actualCount = deck.Count;
+
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [TestMethod]
+        public void DeckKanGeschudWorden() {
+
+        }
+
+        [TestMethod]
+        public void DeckGeeftBovensteKaart() {
+
+        }
+
+        [TestMethod]
+        public void DeckKanBijgevuldWorden() {
+
+        }
+
+        [TestMethod]
+        public void DeckGeeftHoeveelheidKaartenInDeck() {
+
+        }
+
+        [TestMethod]
+        public void IfCardIsSubstracedCountDecreases() {
+
+        }
+    }
+}
