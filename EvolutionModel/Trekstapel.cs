0e5c27b0beeb42c7b084395688b0316e95b1e728
Jarko Dubbeldam <jarkodubbeldam@gmail.com>
2019-05-11 16:07:51 +0200

    added tests and implemented trekstapel

---
+++ EvolutionModel/Trekstapel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Evolution.Model {
+  public class Trekstapel {
+    private readonly Stack<Card> cards;
+
+    public Trekstapel(IEnumerable<Card> cards) {
+      this.cards = new Stack<Card>(cards.Reverse());
+    }
+
+    public int Count => cards.Count;
+
+    public bool TryDraw(out Card drawnCard) {
+      if(cards.Count == 0) {
+        drawnCard = default;
+        return false;
+      } else {
+        drawnCard = cards.Pop();
+        return true;
+      }
+    }
+  }
+}
